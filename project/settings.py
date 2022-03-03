ad5fbcb05589c469f21ca34abaf95fc1fccc2584
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-03-03 21:24:33 +0000

    initial property model

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-ws^_8%ny4*awim04s^@xl)5w*7bvh7ew=zs9b4(nsf)p%ixwsx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Property(models.Model):
+    house_number_or_name = models.CharField(max_length=50, default=None)
+    address = models.CharField(max_length=300, default=None)
+    lon = models.FloatField(default=None)
+    lat = models.FloatField(default=None)
+    beds = models.IntegerField(default=None)
+    bathrooms = models.IntegerField(default=None)
+    schools = models.CharField(max_length=100, default=None)
+    train_stations = models.CharField(max_length=100, default=None)
+    level = models.IntegerField(default=None)
+    short_description_level1 = models.CharField(max_length=300, default=None)
+    short_description_level2 = models.CharField(max_length=300, default=None)
+    short_description_level3 = models.CharField(max_length=300, default=None)
+    long_description_level1 = models.TextField(max_length=3000, default=None)
+    long_description_level2 = models.TextField(max_length=3000, default=None)
+    long_description_level3 = models.TextField(max_length=3000, default=None)
+    images_level1 = models.CharField(max_length=5000, default=None)
+    images_level2 = models.CharField(max_length=5000, default=None)
+    images_level3 = models.CharField(max_length=5000, default=None)
+    base_rate_level1 = models.IntegerField(default=None)
+    base_rate_level2 = models.IntegerField(default=None)
+    base_rate_level3 = models.IntegerField(default=None)
+    level1_improvement_cost = models.IntegerField(default=None)
+    level2_improvement_cost = models.IntegerField(default=None)
+    # owner = models.ForeignKey(
+    #  "users.User",
+    #  related_name="property",
+    #  on_delete = models.CASCADE
+    # )
+    asking_price = models.IntegerField(default=None)
+    # offers = models.ForeignKey(
+    #  "offers.Offer",
+    #  related_name="property",
+    #  on_delete = models.CASCADE
+    # )
+    # investment_history = models.ManyToManyField(
+    #  "transactions.Transaction",
+    #  related_name="property",
+    # )
+    # mortgages = models.ForeignKey(
+    #  "mortgages.Mortgage",
+    #  related_name="property",
+    #  on_delete = models.CASCADE
+    # )
+    # monthly_income
+
+    def __str__(self):
+        return f"{self.house_number_or_name} {self.address}"
