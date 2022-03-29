36afdeb88f0f15eac4b660111ff03c877cdc9934
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-03-29 20:45:54 +0100

    test

--- project/settings.py
+++ project/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import django_on_heroku
 from pathlib import Path
-#import os
+import os
 #import sys
-import redis
+#import redis
 from dotenv import load_dotenv
 load_dotenv()
 
